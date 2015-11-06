f0050952da2e57907c3284f232938353240b7135
filatei <filatei@users.noreply.github.com>
2015-11-06 22:11:00 +0100

    logged calls menu added callertype field added to caller form as required field domain set to customer for now for caller view
    Menus are now relocated directly instead of duplication

---
+++ odoo8/caller/caller_company_img.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# got from https://raw.githubusercontent.com/netjunky-hub/company_setup/master/company_setup/company_image.py
+
+from openerp import api, fields, models
+
+
+class res_company(models, Model):
+    _inherit = "res.company"
+    _name = "res.company"
+
+    def init(self, cr):
+        img = open(os.path.join(os.path.dirname(__file__),
+                                'static', 'src', 'img', 'logo.png'), 'rb').read().encode('base64')
+        cr.execute('UPDATE res_partner SET image=%s WHERE is_company=TRUE', (img,))
+        size = (180, None)
+        cr.execute('UPDATE res_company SET logo_web=%s', (image_resize_image(img, size),))
